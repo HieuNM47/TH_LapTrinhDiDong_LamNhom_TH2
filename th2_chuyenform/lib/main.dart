4d3178700ab53c0a8bd7591986a37252875c468f
HieuNM47 <nmhieucoder@gmail.com>
2021-11-07 00:58:25 +0700

    push code all main

--- th2_chuyenform/lib/main.dart
+++ th2_chuyenform/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: LoadingPage(),
     );
   }
 }
@@ -247,6 +248,199 @@ class PageLoiDangNhap extends StatelessWidget {
                 ],
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+  final _IDControlUse = TextEditingController();
+  final _IDControlPass = TextEditingController();
+  String Us = 'TOB123';
+  String Pass = '123456';
+
+  @override
+  Widget build(BuildContext context) {
+    void Login() {
+      if (_IDControlUse.text == '' || _IDControlPass.text == '') {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Thông báo'),
+                content: Text('Yêu cầu nhập đầy đủ thông tin'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Đóng'),
+                  )
+                ],
+              );
+            });
+      } else if (_IDControlUse.text == Us && _IDControlPass.text == Pass) {
+        //Navigator chuyển tới trang Load + mailBoxes
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoadingPage()));
+        Timer(Duration(seconds: 3), () {});
+      } else {
+        //Navigator chuyển tới trang lỗi đăng nhập, Showdialog chỉ mang tính chất test xử lý đăng nhập
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Thông báo'),
+                content: Text('Đăng nhập không thành công'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Đóng'),
+                  )
+                ],
+              );
+            });
+      }
+    }
+
+    ;
+
+    Widget Imagesection = Image.asset(
+      'images/Email.JPG',
+      width: 150,
+      height: 150,
+      fit: BoxFit.cover,
+    );
+    Widget TextSignln = Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Signln',
+        style: TextStyle(fontSize: 60, color: Colors.white),
+      ),
+    );
+    Widget Textabc = Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Speak, friend, and enter',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+    Widget TextBoxEmail = Container(
+      padding: EdgeInsets.all(15),
+      child: TextField(
+        controller: _IDControlUse,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: 'Email',
+          prefixIcon: Icon(Icons.email, color: Colors.black),
+        ),
+      ),
+    );
+    Widget TextBoxPassword = Container(
+      padding: EdgeInsets.only(top: 15, right: 15, left: 15, bottom: 50),
+      child: TextField(
+        controller: _IDControlPass,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: 'Password',
+          prefixIcon: Icon(Icons.lock, color: Colors.black),
+        ),
+        obscureText: true,
+      ),
+    );
+    Widget ButtonSignin = Container(
+      width: 480,
+      height: 50,
+      // padding: EdgeInsets.all(25),
+      child: OutlinedButton(
+        onPressed: () {
+          Login();
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.black),
+        ),
+        child: Text('SIGN IN', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        //title: Text('Đăng nhập'),
+      ),
+      backgroundColor: Colors.cyan,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Imagesection,
+          TextSignln,
+          Textabc,
+          TextBoxEmail,
+          TextBoxPassword,
+          // SizedBox(
+          //   width: 30,
+          //   height: 30,
+          // ),
+          ButtonSignin,
+        ],
+      )),
+    );
+  }
+}
+
+// Giao diện loading
+class LoadingPage extends StatelessWidget {
+  LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget Imagesection = Image.asset(
+      'images/Email.JPG',
+      width: 150,
+      height: 150,
+      fit: BoxFit.cover,
+    );
+    Widget TextSignln = Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Signln',
+        style: TextStyle(fontSize: 60, color: Colors.white),
+      ),
+    );
+    Widget Textabc = Container(
+      alignment: Alignment.center,
+      child: Text(
+        'Speak, friend, and enter',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+    Widget Load = Container(
+      width: 300,
+      height: 300,
+      child: SpinKitFadingCircle(color: Colors.white),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+      ),
+      backgroundColor: Colors.cyan,
+      body: Center(
+        child: Column(
+          children: [
+            Imagesection,
+            TextSignln,
+            Textabc,
+            Load,
           ],
         ),
       ),
